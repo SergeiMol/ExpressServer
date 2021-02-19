c8bf0e2a74479d32d5a0680832176e41444464ce
SergeiMol <sergei.molohvey@gmail.com>
2021-02-19 15:11:40 +0300

    Create server path of website

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-    origin: "https://modest-neumann-88d173.netlify.app/"
+    origin: "http://localhost:3000"
 };
 
 app.use(cors(corsOptions));
